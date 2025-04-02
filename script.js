6c9222a18bdcde6796aa353636689b79492c0f1e
Chaitanya Patel <cp2263@rit.edu>
2025-04-02 14:47:11 -0700

    Mobile alignment fixes

--- script.js
+++ script.js
@@ -92,3 +92,13 @@ function scrollToTop() {
 //     cursor.style.top = `${e.clientY}px`;
 //   }
 // });
+
+// Header Shrink on Scroll
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 50) {
+    header.classList.add('shrink');
+  } else {
+    header.classList.remove('shrink');
+  }
+});
